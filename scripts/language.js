7593c3d41c2fb95c802f995af22eee2dc025bd22
Natural <jarkurghan@gmail.com>
2024-09-02 06:14:50 +0500

    'experience' translating

--- scripts/language.js
+++ scripts/language.js
@@ -6,6 +6,8 @@ if (lang === "uz") {
     document.querySelector('a[href="#projects"]').textContent = "⊳ Loyihalar";
     document.querySelector("#about>.title").textContent = "Men haqimda";
     document.querySelector("#about>.info").textContent = "Dasturlashga kirib kelishim universitet davrida bo'lgan. Dasturlash sohasiga yaqin bo'lgan axborot xavfsizligi yo'nalishi C++, Python va Web dasturlash bo'yicha boshlang'ich bilimlarimni olishimga sabab bo'lgan. Professional tajribam esa 2021-yil 4-kurslik davrimda boshlangan";
+    document.querySelector("#experience>.title").textContent = "Tajriba";
+    document.querySelector('a[href*="transoxania.uz"] .experience-description') // ...
 } else if (lang === "ru") {
     document.getElementsByClassName("sider-profession")[0].textContent = "Full-stack разработчик";
     document.getElementsByClassName("sider-description")[0].textContent = "Я Full-Stack разработчик с опытом работы 2,5 года";
@@ -14,6 +16,7 @@ if (lang === "uz") {
     document.querySelector('a[href="#projects"]').textContent = "⊳ Проекты";
     document.querySelector("#about>.title").textContent = "Обо мне";
     document.querySelector("#about>.info").textContent = "Я начал заниматься программированием, когда учился в университете. Фокус на информационной безопасности, близкий к области программирования, привел к моим первоначальным знаниям C++, Python и веб-программирования. Мой профессиональный опыт начался на 4-м курсе 2021 года";
+    document.querySelector("#experience>.title").textContent = "Опыт";
 } else if (lang === "en") {
     document.getElementsByClassName("sider-profession")[0].textContent = "Full stack developer";
     document.getElementsByClassName("sider-description")[0].textContent = "I am Full-Stack developer with 2.5 years experience";
@@ -22,4 +25,5 @@ if (lang === "uz") {
     document.querySelector('a[href="#projects"]').textContent = "⊳ Projects";
     document.querySelector("#about>.title").textContent = "About me";
     document.querySelector("#about>.info").textContent = "I started programming when I was studying at the university. An information security focus close to the field of programming led to my initial knowledge of C++, Python and Web programming. My professional experience started during my 4th year of 2021";
+    document.querySelector("#experience>.title").textContent = "Experience";
 }
