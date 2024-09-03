93421050839e2fa543f1ea20fe894d9e4657a827
Natural <jarkurghan@gmail.com>
2024-09-03 10:12:20 +0500

    projects translating - lugat

--- scripts/language.js
+++ scripts/language.js
@@ -15,6 +15,8 @@ if (lang === "uz") {
     document.querySelector('a[href*="digitalgov.uz"] .exp-end').textContent = "2021 dekabr"
     document.querySelector('a[href*="digitalgov.uz"] .experience-description-info').textContent = "DigitalGov Innovation HUB MChJda mentor. Maktab o'qituvchilariga C++ tilida dasturlash asoslaridan dars berganman. Lekin ustozlik davrim uzoq davom etmadi, chunki darslik ikki oyga mo‘ljallangan edi"
     document.getElementsByClassName("basic-of-programming")[0].textContent = "dasturlash asoslari";
+    document.querySelector("#projects>.title").textContent = "Loyihalar";
+    document.querySelector('a[href*="virtual-lugat.uz"] .project-description-info').textContent = "O'zbek tilidagi so'zlar ma'nolarining izohli lug'ati. G'oyasi o'zimga tegishli bo'lgan, har bir kodini o'zim yozgan birinchi proyekt. Proyekt admin va foydalanuvchi qismiga bo'linadi. Admin tomondan so'zlar va so'z haqida ma'lumotlar kiritiladi, foydalanuvchi tomon esa ochiq qidiruv tizimi kabi ishlaydi"
 } else if (lang === "ru") {
     document.getElementsByClassName("sider-profession")[0].textContent = "Full-stack разработчик";
     document.getElementsByClassName("sider-description")[0].textContent = "Я Full-Stack разработчик с опытом работы 2,5 года";
@@ -32,6 +34,8 @@ if (lang === "uz") {
     document.querySelector('a[href*="digitalgov.uz"] .exp-end').textContent = "2021 Декабрь"
     document.querySelector('a[href*="digitalgov.uz"] .experience-description-info').textContent = "Mentor в DigitalGov Innovation HUB ООО. Я преподавал основы программирования на C++ школьным учителям. Мой период наставничества длился недолго, так как учебник был рассчитан на два месяца"
     document.getElementsByClassName("basic-of-programming")[0].textContent = "основы программирования";
+    document.querySelector("#projects>.title").textContent = "Проекты";
+    document.querySelector('a[href*="virtual-lugat.uz"] .project-description-info').textContent = "Толковый словарь значений слов узбекского языка. Это первый проект, где идея принадлежит мне и весь код я написал сам. Проект разделен на административную и пользовательскую часть. На стороне администратора вводятся слова и информация о словах, а на стороне пользователя работает как открытая поисковая система."
 } else if (lang === "en") {
     document.getElementsByClassName("sider-profession")[0].textContent = "Full stack developer";
     document.getElementsByClassName("sider-description")[0].textContent = "I am Full-Stack developer with 2.5 years experience";
@@ -49,4 +53,6 @@ if (lang === "uz") {
     document.querySelector('a[href*="digitalgov.uz"] .exp-end').textContent = "2021 Dec"
     document.querySelector('a[href*="digitalgov.uz"] .experience-description-info').textContent = "Mentor at DigitalGov Innovation HUB LLC. I taught the basics of C++ programming to school teachers. My mentoring period did not last long because the textbook was designed for two months"
     document.getElementsByClassName("basic-of-programming")[0].textContent = "basic of programming";
+    document.querySelector("#projects>.title").textContent = "Projects";
+    document.querySelector('a[href*="virtual-lugat.uz"] .project-description-info').textContent = "An explanatory dictionary of the meanings of words in the Uzbek language. This is the first project where the idea belongs to me and I wrote all the code myself. The project is divided into admin and user part. On the admin side, words and word information are entered, while the user side works like an open search engine"
 }
